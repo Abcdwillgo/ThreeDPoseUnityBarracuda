26cabed4d469c307e7bf067586cbfe01048ecfa7
Abcdwillgo <129616904+Abcdwillgo@users.noreply.github.com>
2023-05-06 11:33:37 -0700

    session 6
    made the ball shooting system
    set up racket
    added reloading

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 65;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void FireBall(Vector3 direction, float power, float maxOffset)
+    {
+        Vector3 offset = new Vector3(Random.Range(-maxOffset, maxOffset), Random.Range(-maxOffset, maxOffset), 0f);
+        rb.position += offset;
+        rb.AddForce(direction * power, ForceMode.Impulse);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<BallScript>() != null || other.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+        Vector3 velocity = rb.velocity;
+        Debug.Log("velocity after hit: " + "m/s");
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/FireTennisBall.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class FireTennisBall : MonoBehaviour
+{
+    public GameObject TennisBall;
+    public float BallSpeed;
+    public Vector3 initPosition = Camera.main.transform.position;
+    public float numBalls = 5;
+    public float maxBalls = 5;
+    public float reloadTime = 2.5f;
+    private bool isReloading = false;
+
+
+    public Text ballsText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numBalls = maxBalls;
+        ballsText.text = $"Balls: {numBalls}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&& !isReloading)
+        {
+            if (numBalls <= 0)
+            {
+                isReloading = true;
+                StartCoroutine(Reloading());
+            }
+            else
+            {
+
+
+                GameObject clone = Instantiate(TennisBall);
+                clone.transform.position = initPosition;
+                Destroy(clone, 7);
+                //to be continued...
+                var ballScript = clone.GetComponent<BallScript>();
+                FireBall(ballScript);
+                numBalls--;
+                ballsText.text = $"Balls: {numBalls}";
+            }
+
+        }
+
+    }
+    private void FireBall(BallScript ball)
+    {
+        Vector3 direction = new Vector3(Random.Range(-0.3f, 0.3f), Random.Range(-0.2f, .2f), Random.Range(0, 1f)).normalized;
+        ball.transform.position = initPosition;
+        ball.FireBall(direction, 10f, BallSpeed);
+
+    }
+
+    public IEnumerator Reloading()
+    {
+        ballsText.text = "Balls: reloading...";
+        yield return new WaitForSeconds(reloadTime);
+        numBalls = maxBalls;
+        isReloading = false;
+        ballsText.text = $"Balls: {numBalls}";
+    }
+
+
+}
