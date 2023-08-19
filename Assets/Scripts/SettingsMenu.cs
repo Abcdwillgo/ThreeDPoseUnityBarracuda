676c2d7cb540cd2ea4320a45161dfec5365357b4
Abcdwillgo <129616904+Abcdwillgo@users.noreply.github.com>
2023-08-18 17:29:54 -0700

    Class 08/18

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+
+public class SettingsMenu : MonoBehaviour {
+    public AudioMixer audioMixer;
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
